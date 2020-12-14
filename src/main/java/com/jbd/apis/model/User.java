6c65ab5009d462147ddd9792795c501e0b463b2e
JBDDAK <54650646+JBDDAK@users.noreply.github.com>
2020-12-14 13:36:40 +0900

    add: model코드 추가한다

--- src/main/java/com/jbd/apis/model/User.java
+++ src/main/java/com/jbd/apis/model/User.java
@@ -1,4 +1,23 @@
 package com.jbd.apis.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+
+    @Column(length = 40, nullable = false)
+    private String email;
+    @Column(length = 20, nullable = false)
+    private String name;
+    @Column(length = 12)
+    private String phone;
 }
